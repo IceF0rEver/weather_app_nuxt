483e012b85b3e8c4ef2e60d76e30dbf4237261a7
IceF0rEver <arnaudorban.pro@gmail.com>
2024-12-18 15:53:10 +0100

    init color-mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/i18n', 
-    '@nuxtjs/tailwindcss'
+    '@nuxtjs/i18n',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode'
   ],
   i18n: {
     locales: [
@@ -12,5 +13,16 @@ export default defineNuxtConfig({
       { code: 'fr', language: 'fr-FR' }
     ],
     defaultLocale: 'fr',
+  },
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '-mode',
+    storage: 'localStorage', // or 'sessionStorage' or 'cookie'
+    storageKey: 'nuxt-color-mode'
   }
 })
