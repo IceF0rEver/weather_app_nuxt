61b540ecdac5887594c6cb09a6ff850d09657027
IceF0rEver <arnaudorban.pro@gmail.com>
2024-12-18 15:35:52 +0100

    init tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/i18n',
+    '@nuxtjs/i18n', 
+    '@nuxtjs/tailwindcss'
   ],
   i18n: {
     locales: [
