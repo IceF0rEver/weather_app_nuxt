c0d9c44673089532608fd588a2f2d7586ff1fa61
IceF0rEver <arnaudorban.pro@gmail.com>
2024-12-23 15:38:14 +0100

    fix: index css

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="dark:bg-gray-800 bg-gray-300">
+  <div class="dark:bg-gray-800 bg-gray-300 min-w-[626px] min-h-screen">
     <div class="mx-auto min-w-[626px] w-1/3">
       <NuxtLayout>
         <NuxtPage/>
