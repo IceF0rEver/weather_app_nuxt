b5ad491a1e74d70a88d141f1b652a51f7dcff499
IceF0rEver <arnaudorban.pro@gmail.com>
2025-04-03 15:58:27 +0200

    fix: code

--- components/Map.vue
+++ components/Map.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="overflow-hidden rounded-lg">
-    <div class="relative w-full h-screen max-h-64 mb-[-35px]">
-      <div class="relative w-full h-full" ref="mapContainer"></div>
+    <div class="overflow-hidden rounded-lg">
+        <div class="relative w-full h-screen max-h-64 mb-[-35px]">
+            <div class="relative w-full h-full" ref="mapContainer">
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script setup lang="ts">
 import type { LocationType } from '@/types/custom-types';
 import { useStorage } from '@vueuse/core';
-
 import { Map, MapStyle, config } from '@maptiler/sdk';
 import '@maptiler/sdk/dist/maptiler-sdk.css';
 import { PrecipitationLayer } from "@maptiler/weather";
@@ -24,31 +24,29 @@ config.apiKey = runtimeConfig.public.API_MAP;
 const layer = new PrecipitationLayer();
 
 onMounted(() => {
-  if (mapContainer.value && current.value.length > 1) {
-    const initialState = {
-      lng: current.value[1].longitude,
-      lat: current.value[1].latitude,
-      zoom: 11,
-    };
+    if (mapContainer.value && current.value.length > 1) {
+        const initialState = {
+            lng: current.value[1].longitude,
+            lat: current.value[1].latitude,
+            zoom: 11,
+        };
 
-    const mapInstance = new Map({
-      container: mapContainer.value,
-      style: MapStyle.DATAVIZ.DEFAULT,
-      center: [initialState.lng, initialState.lat],
-      zoom: initialState.zoom,
-    });
+        const mapInstance = new Map({
+            container: mapContainer.value,
+            style: MapStyle.DATAVIZ.DEFAULT,
+            center: [initialState.lng, initialState.lat],
+            zoom: initialState.zoom,
+        });
 
-    map.value = mapInstance;
+        map.value = mapInstance;
 
-    mapInstance.on('load', () => {
-      mapInstance.addLayer(layer);
-    });
-  }
+        mapInstance.on('load', () => {
+            mapInstance.addLayer(layer);
+        });
+    }
 });
 
 onScopeDispose(() => {
-  map.value?.remove();
+    map.value?.remove();
 });
 </script>
-
-<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,21 @@
 <template>
-  <div class=" flex flex-col dark:bg-gray-800 bg-gray-300">
-    <!-- <div class="absolute w-full h-full dark:bg-black dark:opacity-50"></div> -->
-    <div class="flex flex-col px-4 gap-4">
-      <!-- Météo instantanée -->
-      <Current/>
-      
-      <!-- Prévisions par tranches de 1 heures -->
-      <Hourly/>
-      
-      <!-- Prévisions pour les jours suivants -->
-      <Daily/>
-
-      <div class="mx-auto">
-      <NuxtLink :to="localePath('favorites')">
-        <button
-          class="bg-blue-500 hover:bg-blue-700 text-white h-16 w-16 leading-3 rounded-full shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out text-4xl"
-        >
-        <Icon name="pajamas:file-addition-solid" class="text-gray-100" size="35" />
-        </button>
-      </NuxtLink>
+    <div class=" flex flex-col dark:bg-gray-800 bg-gray-300">
+        <div class="flex flex-col px-4 gap-4">
+            <Current/>
+            <Hourly/>
+            <Daily/>
+            <div class="mx-auto">
+                <NuxtLink :to="localePath('favorites')">
+                    <button
+                    class="bg-blue-500 hover:bg-blue-700 text-white h-16 w-16 leading-3 rounded-full shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out text-4xl"
+                    >
+                        <Icon name="pajamas:file-addition-solid" class="text-gray-100" size="35" />
+                    </button>
+                </NuxtLink>
+            </div>
+        </div>
     </div>
-    </div>
-  </div>
 </template>
-
 <script setup lang="ts">
 const localePath = useLocalePath()
-
 </script>
-<style lang="scss" scoped></style>
