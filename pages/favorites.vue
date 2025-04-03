8ea338d67292f7eb257d0a4c32953b32ea62871e
IceF0rEver <arnaudorban.pro@gmail.com>
2025-04-03 15:57:15 +0200

    refact: favorite

--- pages/favorites.vue
+++ pages/favorites.vue
@@ -1,10 +1,9 @@
 <template>
-  <div class="mx-auto flex flex-col dark:bg-gray-800 bg-gray-300">
-    <div class="flex flex-col px-4 gap-4">
-      <AddFrom/>
-      <FavoritesList/>
+    <div class="mx-auto flex flex-col dark:bg-gray-800 bg-gray-300">
+        <div class="flex flex-col px-4 gap-4">
+            <AddFrom/>
+            <FavoriteList/>
+        </div>
     </div>
-  </div>
 </template>
 <script setup lang="ts"></script>
-<style lang="scss" scoped></style>
